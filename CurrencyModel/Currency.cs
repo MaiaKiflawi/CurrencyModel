b6ded25e882946ad770e92c73ecca8bfcd76a06e
MaiaKiflawi <MaiaKiflawi@gmail.com>
2023-11-22 15:00:26 +0200

    Add project files.

---
+++ CurrencyModel/Currency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CurrencyModel
+{
+    [DataContract]
+    public class Currency
+    {
+        [DataMember]
+        public string Key {  get; set; }
+        [DataMember]
+        public double Rate { get; set; }
+        [DataMember]
+        public double Change { get; set; }
+        [DataMember]
+        public int Unit { get; set; }
+    }
+
+    [CollectionDataContract]
+    public class CurrencyList : List<Currency>
+    {
+        public CurrencyList() { }
+        public CurrencyList(IEnumerable<Currency> List):base(List) { }
+    }
+}
---
+++ CurrencyService/MyCurrencyService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using CurrencyModel;
+using CurrencyBL;
+
+namespace CurrencyService
+{
+    public class MyCurrencyService : ICurrencyService
+    {
+        public CurrencyList GetAllCurrencies()
+        {
+            CurrencyManager manager = new CurrencyManager();
+            return manager.GetCurrencyList();
+        }
+
+        public double Convert(Currency source, Currency dest, double amount)
+        {
+            CurrencyManager manager = new CurrencyManager();
+            return manager.Convert(source, dest, amount);
+        }
+    }
+}
